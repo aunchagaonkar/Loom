b5fb6ff461bf318c55e90f731f729be5133d62c6
Ameya Vijay Unchagaonkar <ameya21unchagaonkar@gmail.com>
2025-09-17 23:52:30 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+  public static void main(String[] args){
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int port = 9092;
+    try {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setReuseAddress(true);
+      clientSocket = serverSocket.accept();
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
+  }
+}
